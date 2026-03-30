1d7c5659bbd0589b73a187c6e43834d594f22fdf
thk67890 <45723894+thk67890@users.noreply.github.com>
2026-03-30 11:05:46 +0000

    update

--- Chapter_Two/Ex1.c
+++ Chapter_Two/Ex1.c
@@ -2,7 +2,7 @@
         directive == #include <stdio.h>
         statement == printf("Parkinson's Law:?\nWork expands so as to ");, printf("fill the time\n");, printf("available for its completion. \n")
    b) what output does the program produce
-        Parkinson's Law:
+        Parkinson's Law:?
         Work expands so as to fill the time
         available for its completion
 
