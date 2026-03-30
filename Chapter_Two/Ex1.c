46a0750c36bae8e32c6157b1200bce4813766258
thk67890 <45723894+thk67890@users.noreply.github.com>
2026-03-30 10:56:18 +0000

    chp2 ex1

---
+++ Chapter_Two/Ex1.c
@@ -0,0 +1,18 @@
+/* a) identify the directives and statements in this program
+        directive == #include <stdio.h>
+        statement == printf("Parkinson's Law:?\nWork expands so as to ");, printf("fill the time\n");, printf("available for its completion. \n")
+   b) what output does the program produce
+        Parkinson's Law:
+        Work expands so as to fill the time
+        available for its completion
+
+
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Parkinson's Law:?\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion. \n")
+}
