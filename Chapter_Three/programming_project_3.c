209ae73ee00cdaa0d2a2c6413bd2e974218c787b
thk67890 <45723894+thk67890@users.noreply.github.com>
2026-04-17 13:11:22 +0000

    update

--- Chapter_Three/programming_project_3.c
+++ Chapter_Three/programming_project_3.c
@@ -5,9 +5,37 @@ int main(void){
     int GSI, GI, PC, IN, CD;
 
     printf("Enter ISBN: ");
-    scanf("%d-%d-%d-%d-%d", GSI, GI, PC, IN, CD);
+    scanf("%d-%d-%d-%d-%d", &GSI, &GI, &PC, &IN, &CD);
 
-    printf("GSI Prefix: %d\nGroup identifier: %d\nPublisher Code: %d\nItem number: %d\nCheck digit: %d", GSI, GI, PC, IN, CD);
+    printf("GSI Prefix: %d\nGroup identifier: %d\nPublisher Code: %d\nItem number: %d\nCheck digit: %d\n", GSI, GI, PC, IN, CD);
 
     return 0;
 }
+
+/*
+Sample Code by Author:
+#include <stdio.h>
+
+int main(void)
+{
+  int prefix, group, publisher, item, check_digit;
+
+  printf("Enter ISBN: ");
+  scanf("%d-%d-%d-%d-%d", &prefix, &group, &publisher, &item, &check_digit);
+
+  printf("GS1 prefix: %d\n", prefix);
+  printf("Group identifier: %d\n", group);
+  printf("Publisher code: %d\n", publisher);
+  printf("Item number: %d\n", item);
+  printf("Check digit: %d\n", check_digit);
+
+  // The five printf calls can be combined as follows:
+
+    //printf("GS1 prefix: %d\nGroup identifier: %d\nPublisher code: %d\nItem number: %d\nCheck digit: %d\n",
+            \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\prefix, group, publisher, item, check_digit);
+  
+
+  return 0;
+}
+
+*/
