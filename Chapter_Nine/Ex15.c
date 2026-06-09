a510f3eb1f5a34d41eeb57455a9b9ec9d543488b
thk67890 <45723894+thk67890@users.noreply.github.com>
2026-06-09 11:22:55 +0000

    Update 0609

---
+++ Chapter_Nine/Ex15.c
@@ -0,0 +1,77 @@
+//The following (rather confusing) function finds the median of three numbers. 
+//Rewrite the function so that it has just one return statement.
+
+/*/
+double median(double x, double y, double z)
+{
+    if(x == y)
+        if(y <= z) return y;
+        else if(x <= z) return z;
+        else return x;
+    if (z <= y) return y;
+    if (x <= z) return x;
+}
+*/
+
+#include <stdio.h>
+
+double median(double x, double y, double z);
+double median_attempt_Two(double x, double y, double z);
+
+int main(void)
+{
+    printf("Median: %0.1f\n", median(5,7,2));
+}
+
+double median(double x, double y, double z)
+{
+    int median;
+    while(1){
+        if (x <= y)
+            if(y <= z){ 
+                median = y;
+                break;
+            }
+
+            else if (x <= z){
+                median = z;
+                break;
+            }
+            else {
+                median = x;
+                break;
+            }
+        
+            if (z <= y) median = y;
+        if (x <= z) median = x;
+        
+        break;
+    }
+    return median;
+}
+
+/*
+Sample ANS
+double median(double x, double y, double z)
+{
+  double result;
+
+  if (x <= y)
+    if (y <= z) result = y;
+    else if (x <= z) result = z;
+    else result = x;
+  else {
+    if (z <= y) result = y;
+    else if (x <= z) result = x;
+    else result = z;
+  }
+
+  return result;
+}
+  
+*/
+
+double median_attempt_Two(double x, double y, double z)
+{
+
+}
