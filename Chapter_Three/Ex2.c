0f095f878a5bee15861769f592308fa1b4ce0201
thk67890 <45723894+thk67890@users.noreply.github.com>
2026-06-13 04:32:11 +0000

    UD

--- Chapter_Three/Ex2.c
+++ Chapter_Three/Ex2.c
@@ -5,9 +5,11 @@ Write calls of printf that display a float variable x in the following formats.
     (c) Fixed-point notation; left-justified in a field of size 8; three digits after the decimal point
     (d) Fixed-point notation; right-justified in a field of size 6; no digits after the decimal point
 */
+/*
 
 (a) printf("%-8.1e", a);
 (b) printf("%10.6e", a);
 (c) printf("%-8.3f", a);
 (d) printf("%6.0f", a);
 
+*/
